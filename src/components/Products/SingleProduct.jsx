0b915ee12ae01bad8104f633805d7d79f68e7f95
OlgaBogush <bohush.olga@gmail.com>
2025-06-26 14:53:19 +0300

    feat: add SingleProduct component

--- src/components/Products/SingleProduct.jsx
+++ src/components/Products/SingleProduct.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react"
+import { useNavigate, useParams } from "react-router"
+
+import { useGetProductQuery } from "../../features/api/apiSlice"
+import { ROUTES } from "../../utils/routes"
 
 const SingleProduct = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const { data, isLoading, isFetching, isSuccess } = useGetProductQuery({ id })
+
+  useEffect(() => {
+    if (!isFetching && !isLoading && !isSuccess) {
+      navigate(ROUTES.HOME)
+    }
+    // eslint-disable-next-line
+  }, [isLoading, isFetching, isSuccess])
+
+  return <div>SingleProduct</div>
 }
 
 export default SingleProduct
